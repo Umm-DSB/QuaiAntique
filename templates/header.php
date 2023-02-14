4cc407ecf343bc07e5d73bdc6d641b635d37e62b
Hayiri Mgomri <hayiri@mgomri.fr>
2023-02-14 08:36:12 +0100

    First files(header,footer and index) and first pictures

---
+++ templates/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Quai Antique</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+
+
+<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+      <img src="images/logo.png" alt="" srcset="">
+    </a>
+
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+      <li><a href="#" class="nav-link px-2 link-secondary">Accueil</a></li>
+      <li><a href="#" class="nav-link px-2 link-dark">Plats</a></li>
+      <li><a href="#" class="nav-link px-2 link-dark">Menus</a></li>
+      <li><a href="#" class="nav-link px-2 link-dark">Horaires</a></li>
+      <li><a href="#" class="nav-link px-2 link-dark">Contact</a></li>
+    </ul>
+
+    <div class="col-md-3 text-end">
+      <button type="button" class="btn btn-dark">Réserver</button>
+      <button type="button" class="btn btn-dark">Se connecter</button>
+    </div>
+</header>
